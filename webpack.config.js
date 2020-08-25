765894d6aa5cef460bc1a196a4a1c2208a8f5137
Sahil Rajput <sahilrajput03@gmail.com>
2020-08-25 15:38:03 +0530

    Cleanup. ~sahil

--- webpack.config.js
+++ webpack.config.js
@@ -5,29 +5,5 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        // My comments=> \. indicates that the adjacent dot should be interpreted as a period or decimal rather than as a wildcard.
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-    ],
-  },
+  }
 };
